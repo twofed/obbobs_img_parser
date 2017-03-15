7e41e09da7c056e878cdfc941c615131ac485552
twofed <whitepapugai@gmail.com>
2017-03-15 09:34:13 +0200

    Its work!

---
+++ main.py
@@ -0,0 +1,27 @@
+from lxml import html
+import requests
+import urllib
+
+i=0
+raz=1 # * 20 img
+path='img/'
+
+class get_boobs():
+    while raz!=i:
+        i += 1
+        page = requests.get('http://oboobs.ru/'+str(i)+'/')
+        tree = html.fromstring(page.content)
+        img = tree.xpath('//img/@src')
+        q=0
+        while len(img)>q:
+            lenq = img[q].find('w/',1,50)
+            name = img[q][lenq+2:]
+            try: img[q].index('_preview', 1, 40)
+            except ValueError:
+                q+=1
+            else:
+                img[q] = img[q].replace('_preview', '')
+                urllib.request.urlretrieve(img[q], path + name)
+                q+=1
+
+
